887917711913792c3fe98f10526952f436d5ccd5
Zhou Xuhui <zhouxuhui@outlook.com>
2014-08-14 13:23:06 +0800

    first commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+Redmine::Plugin.register :prj_tree do
+  name 'Prject Tree'
+  author 'Zhou Xuhui'
+  description 'Replace the project tree with a collapsible tree for project main page'
+  version '0.0.1'
+  url 'https://github.com/xuhui/prj_tree'
+end
